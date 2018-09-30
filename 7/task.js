ff90de322654ac7c7857c4e7f78ee978b2b10ef5
AntonVoronezh <pstut@mail.ru>
2018-09-30 21:04:21 +0300

    7 #6

--- 7/task.js
+++ 7/task.js
@@ -1,34 +1,31 @@
 export const car = (num) => {
     if (num === 1) {
       return 0;
-   } else {
-  let count = 0;
-  let acc = num;
+    }
+    let count = 0;
+    const acc = num;
   
-    const f = acc => {
+    const f = (acc) => {
       count += 1;
-      acc = acc / 2;
+      acc /= 2;
       return acc % 2 === 0 ? f(acc) : count;
-      //console.log(acc);
-      }
+    };
     return f(acc);
-   }
   };
   
-  export const cdr = num => {
+  export const cdr = (num) => {
     if (num === 1) {
       return 0;
-      } else {
-        let count = 0;
-        let acc = num / 2 ** car(num);
-        const f = acc => {
-            count += 1;
-            acc = acc / 3;
-            return acc % 3 === 0 ? f(acc) : count;
-            }
-        return f(acc);
-      }
-  }
+    }
+    let count = 0;
+    const acc = num / 2 ** car(num);
+    const f = (acc) => {
+      count += 1;
+      acc /= 3;
+      return acc % 3 === 0 ? f(acc) : count;
+    };
+    return f(acc);
+  };
   
-  export const cons = (x, y) => x === 0 && y === 0 ? 1: (2 ** x) * (3 ** y);
+  export const cons = (x, y) => (x === 0 && y === 0 ? 1 : (2 ** x) * (3 ** y));
   
