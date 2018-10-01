6f1a5ae7d25cc319b8d10896f7d94e610cb05184
AntonVoronezh <pstut@mail.ru>
2018-10-01 20:34:48 +0300

    8 #5

---
+++ 8/task.js
@@ -0,0 +1,40 @@
+import { l, cons, head, tail, isEmpty, toString as listToString } from 'hexlet-pairs-data'; // eslint-disable-line
+
+// BEGIN (write your solution here)
+export const has = (numbers, num) => {
+  if (isEmpty(numbers) === false) {
+        if (head(numbers) === num) { 
+          return true;
+        }
+  return has(tail(numbers), num);
+  }
+return false;
+}
+
+export const reverse = (numbers) => {
+
+  let acc = l(); console.log(acc + ' acc');
+
+  const iter = (numbers) => {
+      if (isEmpty(numbers) === false) {
+        acc = cons(head(numbers), acc);
+        console.log(acc + ' acc');
+        return iter(tail(numbers));  
+      }
+      return acc;
+  }
+  return iter(numbers);
+}
+
+export const concat = (numbers, numbers2) => {
+
+  const recurs = (n) => {
+    if (isEmpty(n) === true) {
+      return numbers2;
+    } else {
+      return cons(head(n), recurs(tail(n)));   
+    }
+  }
+return recurs(numbers);
+}
+
